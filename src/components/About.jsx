993906329f6d0f03f33e9951c90e336e036dd5e4
Ashmil114 <ashmilk114@gmail.com>
2023-09-20 23:22:56 -1200

    second commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,19 +5,21 @@ import avar from '../assets/images/Ellipse 1.png'
 
 const About = () => {
   return (
-    <div className='h-screen ' id='about' >
-        <h1 className='text-[100px] font-[600] invisible lg:visible absolute text-center w-full -mt-6'><span className='text-[#8D8D8D]'>.</span>..</h1>
-        <div className='lg:flex w-full h-screen'>
-        <svg className='ml-[17.45%]  invisible lg:visible  absolute' xmlns="http://www.w3.org/2000/svg" width="440" height="150" viewBox="0 0 440 150" fill="none"><path d="M438 0V52H2V280" stroke="#EBFF00" stroke-width="4"/></svg>
-            <div className='lg:w-[50%]  lg:h-screen lg:flex lg:justify-center lg:flex-col h-screen flex '>
-                <h1 className='lg:text-[360px] lg:font-[600]  text-[#EBFF00] lg:ml-[1rem] lg:p-1 text-[120px] lg:relative absolute -bottom-[100%] lg:bottom-0 ml-3 md:text-[150px]'>01</h1>
-                <h1 className='lg:text-[40px] lg:-mt-[7rem] lg:ml-[7rem] text-[40px] w-full lg:relative  flex justify-center lg:justify-normal mt-9 '>ABOUT ME</h1>
-                <svg className='lg:ml-[34%] invisible lg:visible ' xmlns="http://www.w3.org/2000/svg" width="4" height="192" viewBox="0 0 4 192" fill="none"><path d="M2 0V192" stroke="#EBFF00" stroke-width="4"/></svg>
-            </div>
-            <div className='lg:w-[50%]  lg:h-screen lg:flex lg:flex-col lg:items-center lg:justify-center lg:relative absolute -bottom-[75%]   lg:-bottom-0 flex  flex-col w-full  items-center ' >
-                <img src={avar} alt=""  className='lg:w-[314px] lg:h-[283px] lg:m-5 w-[224px] h-[203px] mb-2 mt-2 p-4 md:w-[284px] md:h-[253px] md:m-[1rem]  '/>
-                <h1 className='lg:m-5 mt-3 ml-8 mr-8 mb-3 text-justify md:m-[1rem] text-[10px] lg:text-[20px] md:text-[15px]'>I'm a full stack developer with a passion for creating dynamic and responsive web applications . I'm constantly looking for ways to integrate ML algorithms into my projects and improve the user experience .with my skills and experience.</h1>
-            </div>
+    <div  >
+        <div className='h-screen ' id='about'>
+          <h1 className='text-[100px] font-[600] invisible lg:visible absolute text-center w-full -mt-6'><span className='text-[#8D8D8D]'>.</span>..</h1>
+          <div className='lg:flex w-full h-screen'>
+          {/* <svg className='ml-[17.45%]  invisible lg:visible  absolute' xmlns="http://www.w3.org/2000/svg" width="440" height="150" viewBox="0 0 440 150" fill="none"><path d="M438 0V52H2V280" stroke="#EBFF00" stroke-width="4"/></svg> */}
+              <div className='lg:w-[50%]  lg:h-screen lg:flex lg:justify-center lg:flex-col h-screen flex '>
+                  <h1 className='lg:text-[360px] lg:font-[600]  text-[#EBFF00] lg:ml-[1rem] lg:p-1 text-[120px] lg:relative absolute -bottom-[100%] lg:bottom-0 ml-3 md:text-[150px]'>01</h1>
+                  <h1 className='lg:text-[40px] lg:-mt-[7rem] lg:ml-[7rem] text-[40px] w-full lg:relative  flex justify-center lg:justify-normal mt-9 '>ABOUT ME</h1>
+                  {/* <svg className='lg:ml-[34%] invisible lg:visible ' xmlns="http://www.w3.org/2000/svg" width="4" height="192" viewBox="0 0 4 192" fill="none"><path d="M2 0V192" stroke="#EBFF00" stroke-width="4"/></svg> */}
+              </div>
+              <div className='lg:w-[50%]  lg:h-screen lg:flex lg:flex-col lg:items-center lg:justify-center lg:relative absolute -bottom-[75%]   lg:-bottom-0 flex  flex-col w-full  items-center ' >
+                  <img src={avar} alt=""  className='lg:w-[314px] lg:h-[283px] lg:m-5 w-[224px] h-[203px] mb-2 mt-2 p-4 md:w-[284px] md:h-[253px] md:m-[1rem]  '/>
+                  <h1 className='lg:m-5 mt-3 ml-8 mr-8 mb-3 text-justify md:m-[1rem] text-[10px] lg:text-[20px] md:text-[15px]'>I'm a full stack developer with a passion for creating dynamic and responsive web applications . I'm constantly looking for ways to integrate ML algorithms into my projects and improve the user experience .with my skills and experience.</h1>
+              </div>
+          </div>
         </div>
     </div>
   )
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
             <h1 className='text-[60px] md:text-[80px] lg:text-[100px] md:-mr-[100px] lg:-mr-[80px]'>ASHMIL K</h1>
             <h1 className='lg:text-[22px] md:text-[12px] text-[10px]'>FULLSTACK DEVELOPER // PYTHON DEVELOPER</h1>
             <a href="#about"><button className='bg-white text-black text-center font-semibold lg:font-bold outline-none hover:bg-[#EBFF00] hover:text-white w-[80px] h-[30px] rounded-[100px] mt-[50px]  md:w-[80px] md:h-[35px] md:rounded-[100px] md:mt-[50px] lg:w-[198px] lg:h-[84px] lg:rounded-[100px] lg:mt-[50px]'>MORE</button></a>
-            <svg className='invisible lg:visible ' xmlns="http://www.w3.org/2000/svg" width="4" height="160" viewBox="0 0 4 160" fill="none"><path d="M2 0V160" stroke="#EBFF00" stroke-width="4"/></svg>
+            {/* <svg className='invisible lg:visible ' xmlns="http://www.w3.org/2000/svg" width="4" height="160" viewBox="0 0 4 160" fill="none"><path d="M2 0V160" stroke="#EBFF00" stroke-width="4"/></svg> */}
         </div>
       </div>
     </div>
